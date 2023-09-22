6b8755df153b001656681544eae78359e32f7c46
Wissarut Kanasub <wissarut.dev@gmail.com>
2023-09-22 14:20:11 +0700

    Index view show Available polkls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Questions
 
 def index(request):
-    return HttpResponse("<h1>hello world</h1>")
+    all_question = Questions.objects.all
+    return render(request, "index.html", {"all_question": all})
